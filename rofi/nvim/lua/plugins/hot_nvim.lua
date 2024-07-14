34ff1acaa10e23d82dc76fb516c560ab7332d3f8
ChaseSunstrom <casunstrom@gmail.com>
2024-07-13 20:56:55 -0500

    Fixed

---
+++ rofi/nvim/lua/plugins/hot_nvim.lua
@@ -0,0 +1,13 @@
+-- No example configuration was found for this plugin.
+--
+-- For detailed information on configuring this plugin, please refer to its
+-- official documentation:
+--
+--   https://github.com/sachinsenal0x64/hot.nvim
+--
+-- If you wish to use this plugin, you can optionally modify and then uncomment
+-- the configuration below.
+
+return {
+  -- "sachinsenal0x64/hot.nvim"
+}
